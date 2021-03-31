e7123f14d41c2b2183dbe2508d6614f14f07348b
Nick Baliesnyi <nikita.balesny@gmail.com>
2021-03-31 11:42:19 +0300

    add initial structure

---
+++ go.mod
@@ -0,0 +1,7 @@
+module clock_sync_golang
+
+go 1.15
+
+require (
+	github.com/stretchr/testify v1.7.0
+)
---
+++ src/mock.go
@@ -0,0 +1,7 @@
+package src
+
+import "fmt"
+
+func Hello(name string) string {
+	return fmt.Sprintf("Hello, %s", name)
+}
